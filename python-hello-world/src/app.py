8055f0d8eb8343aac6ddcc0ae1ddaabff5eb6a80
WGBitencourt <wendell.bitencourt@oito.srv.br>
2023-11-23 19:15:01 -0300

    update

---
+++ python-hello-world/src/app.py
@@ -0,0 +1,15 @@
+# Create venv enviroment, use git bash
+# python -c "import site; print(site.getsitepackages()[0])"
+# python -m site //list hierarchy where python will be search for interpreter
+# python -m venv .venv //create path venv
+# source .venv/Scripts/activate //activate enviroment venv
+# echo $VIRTUAL_ENV //show current path virtual venv
+# python -m site //list hierarchy where python will be search for interpreter
+# pip list //list dependences
+# $ pip install -r requirements.txt //install dependences inside txt
+
+print("Hello world!!!!!!!")
+
+idade = 200
+
+print(idade)
