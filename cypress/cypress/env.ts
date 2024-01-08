eb1da5513a902b460e62cc45dc250dc3cd12dcbb
WGBitencourt <wendell.bitencourt@oito.srv.br>
2024-01-08 11:19:52 -0300

    update

--- cypress/cypress/env.ts
+++
@@ -1,7 +0,0 @@
-
-export const credentials = {
-  email: Cypress.env('CYPRESS_USER_EMAIL'),
-  password: Cypress.env('CYPRESS_USER_PASSWORD'),
-}
-
-export const baseUrl = Cypress.env('CYPRESS_BASE_URL');
