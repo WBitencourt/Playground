63eaa9f2ea8cf3a6bbb2781260ffd155af4c4f0f
WGBitencourt <wendell.bitencourt@oito.srv.br>
2024-02-13 18:31:16 -0300

    add aadjust tailwind project

--- react-native-expo/src/app/index.tsx
+++ react-native-expo/src/app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 
 export default function Home() {
   return (
-    <View>
+    <View className="bg-green-500 flex-1 items-center justify-center">
       <Text>Home</Text>
     </View>
   );
